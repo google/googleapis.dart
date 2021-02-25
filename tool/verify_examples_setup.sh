0de4246400df0d28a8a35c6d4ecc26e0d508fa1c
Kevin Moore <kevmoo@google.com>
2021-02-25 12:12:13 -0800

    Test _test_package

--- tool/verify_examples_setup.sh
+++ tool/verify_examples_setup.sh
@@ -7,7 +7,7 @@ pub run test test/client_generator_test.dart
 dart bin/generate.dart package --package-name test_package -i ../_test/rest -o ../_test_package
 popd
 
-cp _test/mono_pkg.yaml _test_package
+cp resources/test_pkg_mono_pkg.yaml _test_package/mono_pkg.yaml
 
 pushd _test_package
 pub get
