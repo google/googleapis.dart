5e21f9671bd379b607ad95c965828c0867e80f60
Kevin Moore <kevmoo@users.noreply.github.com>
2021-03-04 10:13:29 -0800

    Last commit before publish (#185)

--- discoveryapis_generator/lib/src/apis_package_generator.dart
+++ discoveryapis_generator/lib/src/apis_package_generator.dart
@@ -181,16 +181,16 @@ ${requestHeadersField(pubspec.version)}
     sink.writeln('dev_dependencies:');
     writeDependencies(pubspec.devDependencies);
 
-    // TODO: remove this when the null safe version is published!
-    sink.writeln('dependency_overrides:');
-    writeDependencies({
-      '_discoveryapis_commons': {'path': _commonsDirRelativePath},
-    });
+    // Uncomment when aligning development with changes to commons
+    // sink.writeln('dependency_overrides:');
+    // writeDependencies({
+    //   '_discoveryapis_commons': {'path': commonsDirRelativePath},
+    // });
   }
 
   /// Returns the relative path from [packageFolderPath] to the commons package
   /// directory.
-  String get _commonsDirRelativePath {
+  String get commonsDirRelativePath {
     const commonsDir = 'discoveryapis_commons';
 
     final outputPackageDir =
