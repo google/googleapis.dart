0545d633b541c1897b794b7fe7b6620c35f77a96
Jonas Finnemann Jensen <jonasfj@google.com>
2021-05-17 13:39:29 +0200

    Suggest using ADC

--- resources/googleapis/examples/main.dart
+++ resources/googleapis/examples/main.dart
@@ -1,20 +1,10 @@
 import 'package:googleapis/storage/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
 
-final _credentials = ServiceAccountCredentials.fromJson(r'''
-{
-  "private_key_id": ...,
-  "private_key": ...,
-  "client_email": ...,
-  "client_id": ...,
-  "type": "service_account"
-}
-''');
-
-const _scopes = [StorageApi.devstorageReadOnlyScope];
-
 Future<void> main() async {
-  final httpClient = await clientViaServiceAccount(_credentials, _scopes);
+  final httpClient = await clientViaApplicationDefaultCredentials(scopes: [
+    StorageApi.devstorageReadOnlyScope,
+  ]);
   try {
     final storage = StorageApi(httpClient);
 
