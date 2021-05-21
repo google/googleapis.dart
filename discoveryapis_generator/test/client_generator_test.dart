a338626714d05040b8ee7327728457ad4b5b9edf
Kevin Moore <kevmoo@google.com>
2021-05-21 10:49:29 -0700

    directive fix

--- discoveryapis_generator/test/client_generator_test.dart
+++ discoveryapis_generator/test/client_generator_test.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
 // Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:discoveryapis_generator/clientstub_generator.dart';
