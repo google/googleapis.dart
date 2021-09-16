0d07be9133c01900307017a6394e8780c71c8438
Kevin Moore <kevmoo@google.com>
2021-09-16 13:07:49 -0700

    Update verify example script

--- tool/verify_examples_setup.sh
+++ tool/verify_examples_setup.sh
@@ -3,12 +3,12 @@
 rm _test/*.dart
 
 pushd discoveryapis_generator
-pub run test test/client_generator_test.dart
+dart test test/client_generator_test.dart
 dart bin/generate.dart package --package-name test_package -i ../_test/rest -o ../_test_package
 popd
 
 cp resources/test_pkg_mono_pkg.yaml _test_package/mono_pkg.yaml
 
 pushd _test_package
-pub get
+dart pub get
 popd
