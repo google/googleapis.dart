f4452b01bc4991805b6cfccc4fb0112a86f4d70f
Kevin Moore <kevmoo@google.com>
2021-10-08 10:44:58 -0700

    generate test package

---
+++ _test_package/lib/shared.dart
@@ -0,0 +1,26 @@
+library $shared;
+
+// ignore_for_file: camel_case_types
+// ignore_for_file: comment_references
+// ignore_for_file: file_names
+// ignore_for_file: library_names
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: prefer_expression_function_bodies
+// ignore_for_file: prefer_interpolation_to_compose_strings
+// ignore_for_file: unnecessary_brace_in_string_interps
+// ignore_for_file: unnecessary_lambdas
+// ignore_for_file: unnecessary_string_interpolations
+
+import 'dart:core' as core;
+
+/// A reusable empty messages.
+class $Empty {
+  $Empty();
+
+  $Empty.fromJson(
+      // ignore: avoid_unused_constructor_parameters
+      core.Map json);
+
+  core.Map<core.String, core.dynamic> toJson() => {};
+}
