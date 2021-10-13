e2a81938dd55bbad53de78c4cd864c033f93bc49
Kevin Moore <kevmoo@google.com>
2021-10-12 17:43:17 -0700

    auth: Fix grammar and links in library docs

--- googleapis_auth/lib/googleapis_auth.dart
+++ googleapis_auth/lib/googleapis_auth.dart
@@ -2,10 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// ignore_for_file: comment_references
+
 /// Contains common libraries used across the package.
 ///
-/// In most cases, you'll want to import any use either
-/// `auth_io.dart` or `auth_browser.dart` depending on your platform.
+/// In most cases, you'll want to import either
+/// [auth_io] or [auth_browser] depending on your platform.
 /// {@canonicalFor access_credentials.AccessCredentials}
 /// {@canonicalFor access_token.AccessToken}
 /// {@canonicalFor auth_client.AuthClient}
