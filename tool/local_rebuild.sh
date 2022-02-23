e06781877382ad77f908e640a510bbd9292ab359
Kevin Moore <kevmoo@google.com>
2022-02-23 14:27:19 -0800

    update usage of `pub` command

--- tool/local_rebuild.sh
+++ tool/local_rebuild.sh
@@ -6,12 +6,12 @@ cp generated/googleapis/lib/discovery/v1.dart discoveryapis_generator/lib/src/ge
 
 echo 'starting on googleapis'
 pushd generated/googleapis || exit
-pub get
+dart pub get
 popd || exit
 
 pwd
 
 echo 'starting on googleapis_beta'
 pushd generated/googleapis_beta || exit
-pub get
+dart pub get
 popd || exit
