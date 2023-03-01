e3c8fccac3c82b8de8f49ac70f9e6e1a4ac15639
Kevin Moore <kevmoo@users.noreply.github.com>
2023-03-01 12:30:05 -0800

    no version: publish_to: none (#513)

--- _test_package/lib/src/user_agent.dart
+++ _test_package/lib/src/user_agent.dart
@@ -2,6 +2,6 @@ import 'package:_discoveryapis_commons/_discoveryapis_commons.dart' as commons;
 
 /// Request headers used by all libraries in this package
 final requestHeaders = {
-  'user-agent': 'google-api-dart-client/0.1.0-dev',
-  'x-goog-api-client': 'gl-dart/${commons.dartVersion} gdcl/0.1.0-dev',
+  'user-agent': 'google-api-dart-client/unknown',
+  'x-goog-api-client': 'gl-dart/${commons.dartVersion}',
 };
