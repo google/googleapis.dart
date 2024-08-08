b8a96d43ca20d1a47ec302cf1fdc8d64ac94fd03
Kevin Moore <kevmoo@google.com>
2024-08-08 15:50:13 -0700

    oops

--- tool/verify_examples_setup.sh
+++ tool/verify_examples_setup.sh
@@ -8,6 +8,7 @@ dart bin/generate.dart package --package-name test_package --package-resolution 
 popd
 
 cp resources/test_pkg_mono_pkg.yaml _test_package/mono_pkg.yaml
+cp resources/test_pkg_analysis_options.yaml _test_package/analysis_options.yaml
 
 pushd _test_package
 dart pub get
