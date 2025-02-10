9e0e7a6418561cd82e92684363d3e55ab2c95294
Kevin Moore <kevmoo@google.com>
2025-02-10 12:24:57 -0800

    [auth] move browser libraries around to improve clarity

--- googleapis_auth/lib/auth_browser.dart
+++ googleapis_auth/lib/auth_browser.dart
@@ -5,6 +5,6 @@
 export 'package:google_identity_services_web/oauth2.dart' show CodeResponse;
 
 export 'googleapis_auth.dart';
-export 'src/authentication_exception.dart' show AuthenticationException;
-export 'src/oauth2_flows/token_model.dart'
+export 'src/browser/authentication_exception.dart' show AuthenticationException;
+export 'src/browser/token_model.dart'
     show requestAccessCredentials, requestAuthorizationCode, revokeConsent;
