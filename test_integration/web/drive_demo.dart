3f661da20a657029d4eddbaa63380fabe1566aad
Kevin Moore <kevmoo@google.com>
2026-02-04 18:08:19 -0800

    more headers

--- test_integration/web/drive_demo.dart
+++ test_integration/web/drive_demo.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // A solution for https://github.com/google/googleapis.dart/issues/366
 
 import 'dart:async';
